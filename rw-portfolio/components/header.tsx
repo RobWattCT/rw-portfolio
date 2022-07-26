bc38587d680719977bd9b65651f1210d0a13c41f
Rob Watt <rob@cloudbreakdigital.com>
2022-07-26 02:37:47 +0200

    Update header.tsx

--- rw-portfolio/components/header.tsx
+++ rw-portfolio/components/header.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Header: React.FC = () => (
   <header className="bg-slate-800 sm:h-20 py-5 sm:sticky top-0">
-    <div className="max-w-7xl mx-auto px-6">
-      <div className="w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center">
+    <div className="max-w-2xl mx-auto px-6">
+      <div className="w-full flex flex-col sm:flex-row justify-center items-center">
         <div className="flex flex-col sm:flex-row items-center mb-4 sm:mb-0">
           <img src="/robwatt-logo-white.png" className="w-14" alt="Rob Watt" />
           <div className="sm:ml-8 flex space-x-5 text-center">
